6a030a2eb0311915b654c5b2a6067873aa08f892
Ryan Mohrman <ryan.mohrman@gmail.com>
2016-07-09 11:26:48 -0600

    working with middleware to log requests

--- app.js
+++ app.js
@@ -4,6 +4,12 @@ var path = require('path');
 
 app.set('port', 3000);
 
+// using middleware to log all requests that come to the application
+app.use(function(req, res, next) {
+	console.log(req.method, req.url);
+	next();
+});
+
 // express will now automatically look in the public folder for files
 // this means that we can (and will) get rid of the root route
 app.use(express.static(path.join(__dirname, 'public')));
