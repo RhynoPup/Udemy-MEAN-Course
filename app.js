1926c87fc90e0c74599c0ac7daed38f5d9032e0a
Ryan Mohrman <ryan.mohrman@gmail.com>
2016-07-09 12:52:08 -0600

    updated to use form-body for posting new hotels

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+var bodyParser = require('body-parser');
 
 var routes = require('./api/routes');
 
@@ -15,6 +16,9 @@ app.use(function(req, res, next) {
 // express will now automatically look in the public folder for files
 // this means that we can (and will) get rid of the root route
 app.use(express.static(path.join(__dirname, 'public')));
+
+// setting the extended option to false forces the form body to only use strings and arrays
+app.use(bodyParser.urlencoded({ extended : false }));
 
 app.use('/api', routes);
 
